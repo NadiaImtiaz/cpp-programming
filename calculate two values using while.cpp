8fc4b8ce034eab5cd7ac3adfd36f9e46f4f08f57
NadiaImtiaz <118732622+NadiaImtiaz@users.noreply.github.com>
2022-11-22 22:03:28 +0500

    Create calculate two values using while.cpp

---
+++ calculate two values using while.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int valueOne;
+	int valueTwo;
+	int i=1;
+	while(i==1)
+	{
+		cout<<"Please enter value number one=";
+		cin>>valueOne;
+		cout<<"Please enter value number Two=";
+		cin>>valueTwo;
+		cout<<"The Result is "<<valueOne+valueTwo<<endl;
+		cout<<"Press 1 to continue any other key to stop= ";
+		cin>>i;		
+	}	
+}
