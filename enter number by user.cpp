88dc9f5014c9699ee075594930521bbf98612a7c
NadiaImtiaz <118732622+NadiaImtiaz@users.noreply.github.com>
2022-11-22 14:19:57 +0500

    Create enter number by user.cpp

---
+++ enter number by user.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int number,limit;
+	cout<<"Enter the Number: ";
+	cin>>number;
+	cout<<"Enter the limit: ";
+	cin>>limit;
+	
+	int i;
+	for(i=1; i<=20; i++)
+	{
+		cout<<"5*"<<i<<"="<<5*i<<endl;
+	}
+
+}
