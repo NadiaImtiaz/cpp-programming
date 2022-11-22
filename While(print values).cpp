5d992fd415fc3870443abc07f24547f49e5538ec
NadiaImtiaz <118732622+NadiaImtiaz@users.noreply.github.com>
2022-11-22 22:12:42 +0500

    Create While(print values).cpp

---
+++ While(print values).cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i=1;
+	while(i==1)
+	{
+		cout<<"while loop is working "<<endl;
+		cout<<"Press 1 to continue any other key to stop"<<endl;
+		cin>>i;		
+	}	
+}
